8e634a31cb0328fd100ba553e8c7511ce5cfbdbc
Hoàng Phương <101681993+HoangPhuong22@users.noreply.github.com>
2022-05-13 17:54:07 +0700

    Update B11_SapXepCacTuTrongXau1.cpp

--- B11_SapXepCacTuTrongXau1.cpp
+++ B11_SapXepCacTuTrongXau1.cpp
@@ -1,6 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+bool cmp(string a , string b)
+{
+    if(a.size() != b.size())
+    {
+        return a.size() < b.size();
+    }
+    return a < b;
+}
 int main()
 {
     string s ; getline(cin , s);
@@ -15,14 +22,7 @@ int main()
     sort(v.begin() , v.end());
     for(string x : v) cout << x << " ";
     cout <<endl;
-    sort(v.begin() , v.end() , [] (string a , string b)->bool
-    {
-        if(a.size() != b.size())
-        {
-            return a.size() < b.size();
-        }
-        return a < b;
-    });
+    sort(v.begin() , v.end() , cmp);
     for(string x : v)
     {
         cout << x << " ";
