993587a6fed87a774e1d27e264923aa3aca5e0dc
Hoàng Phương <101681993+HoangPhuong22@users.noreply.github.com>
2022-05-13 18:21:08 +0700

    Update B4_KiTuXuatHienNhieuNhatTrongXau.cpp

--- B4_KiTuXuatHienNhieuNhatTrongXau.cpp
+++ B4_KiTuXuatHienNhieuNhatTrongXau.cpp
@@ -1,36 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
-    string s ; cin >> s;
-    map<char,int>mp;
+    string s ; cin >>s;
+    int a[256] = {0};
     for(int i = 0 ; i < s.length() ; i++)
     {
-        mp[s[i]]++;
+        a[s[i]]++;
     }
-    vector<pair<char,int>>v;
-    for(auto x : mp)
+    int Max = 0 , Min = 1e5;
+    for(int i = 0 ; i < 256 ; i++)
     {
-        v.push_back({x.first , x.second});
-    }
-    sort(v.begin() , v.end() , [](pair<char,int> a , pair<char,int>b)->bool
-    {
-        if(a.second == b.second)
+        if(a[i] != 0)
         {
-            return a.first < b.first;
-        }
-        return a.second < b.second;
-    });
-    cout << v[v.size() - 1].first << " " << v[v.size() - 1].second << endl;
-    int res = v[0].second;
-    char kq = v[0].first;
-    for(int i = 1 ; i < v.size() ; i++)
-    {
-        if(v[i].second == res)
-        {
-            kq = v[i].first;
+            Max = max(a[i], Max);
+            Min = min(a[i] , Min);
         }
     }
-    cout << kq << " " << res;
+    for(int i = 255 ; i>= 0 ; i--)
+    {
+        if(a[i] == Max)
+        {
+            cout << (char) i << " " << a[i] ;
+            break;
+        }
+    }
+    cout <<endl;
+    for(int i = 255 ; i>= 0 ; i--)
+    {
+        if(a[i] == Min)
+        {
+            cout << (char) i << " " << a[i] ;
+            break;
+        }
+    }
 }
