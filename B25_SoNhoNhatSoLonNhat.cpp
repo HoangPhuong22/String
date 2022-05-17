d5510a17cbf006c1b7ab8cc08aa6828a96f99739
Hoàng Phương <101681993+HoangPhuong22@users.noreply.github.com>
2022-05-17 20:22:08 +0700

    Update B25_SoNhoNhatSoLonNhat.cpp

--- B25_SoNhoNhatSoLonNhat.cpp
+++ B25_SoNhoNhatSoLonNhat.cpp
@@ -1,59 +1,59 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+using ll = long long;
+const int MOD = 1e9 + 7;
 
-void Max(int m , int s)
+void Max(int n , int s)
 {
-
-        string res = "";
-        while(m--)
+    string res = "";
+    for(int i = 0 ; i < n; i++)
+    {
+        if(s >= 9)
         {
-            if(s >= 9)
-            {
-                res += to_string(9);
-                s -= 9;
-            }
-            else
-            {
-                res += to_string(s);
-                s = 0;
-            }
+            res = "9" + res;
+            s -= 9;
         }
-        cout << res;
-
+        else
+        {
+            res = res + to_string(s);
+            s = 0;
+        }
+    }
+    cout << res;
 }
-void Min(int m , int s)
+void Min(int n , int s)
 {
-        string res = "";
-        m--;
-        s--;
-        while(m--)
+    string res = "";
+    --n , --s;
+    for(int i = 0 ; i < n;i++)
+    {
+        if(s >= 9)
         {
-            if(s >= 9)
-            {
-                res += to_string(9);
-                s -= 9;
-            }
-            else
-            {
-                res = to_string(s) + res;
-                s = 0;
-            }
+            res += "9";
+            s -= 9;
         }
-        ++s;
-        res = to_string(s) + res;
-        cout << res;
+        else
+        {
+            res = to_string(s) + res;
+            s = 0;
+        }
+    }
+    s += 1;
+    res = to_string(s) + res;
+    cout << res;
 }
 int main()
 {
-    int m , s ; cin >> m >> s;
-    if(m * 9 < s || s == 0)
+    int n , s ; cin >> n >> s;
+    if(n * 9 < s)
     {
-        cout << "NOT FOUND";
+        cout << "NOT FOUND" ;
+        return 0;
     }
     else
     {
-         Min(m , s);
+        Min(n , s);
         cout << endl;
-        Max(m , s);
+        Max(n , s);
     }
 }
